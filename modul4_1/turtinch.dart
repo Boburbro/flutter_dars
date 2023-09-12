0e163eee1f6adddc3d231ed2f219e186b88891ed
Boburbro <boburotaboyev0@gmail.com>
2023-09-12 07:45:32 +0500

    update

---
+++ modul4_1/turtinch.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(){
+  List<int> number = [];
+  print("Raqam kiriting!");
+
+  var raqam = int.parse(stdin.readLineSync()!);
+
+  for (int n = 1 ; n < raqam; n++){
+    if (raqam % n == 0){
+    number.add(n);
+    }
+  }
+  print(number);
+}
