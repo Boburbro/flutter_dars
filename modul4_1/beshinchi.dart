b2d401b975025a1d5dab547c8561ac16e08d0878
Boburbro <boburotaboyev0@gmail.com>
2023-09-12 19:51:52 +0500

    update

--- modul4_1/beshinchi.dart
+++ modul4_1/beshinchi.dart
@@ -1,18 +1,11 @@
 
 void main(){
-  List<int> a = [1,2,3,4,8,57,7,1,2,8,8,3,46,56,1,2,3,4,8,57,7];
-  List<int> b = [1,2,8,8,3,46,56,1,2,3,4,8,57,7,1,2,6,7,88,8];
+  List<int> a = [1,2,3,4,8,57,7,7];
+  List<int> b = [1,2,8,8,3,46,56];
   List<int> c = [];
-
   for (var aa in a) {
-    for (var bb in b) {
-      if (aa == bb){
-        if (c.contains(aa)){
-          continue;
-        }else{
-          c.add(aa);
-        }
-      }
+    if (b.contains(aa)){
+      c.add(aa);
     }
   }
   print(c);
