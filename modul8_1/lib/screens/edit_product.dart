de15d5d41d2fb1fead501e41dc703869d37cb420
Boburbro <boburotaboyev0@gmail.com>
2023-12-05 20:38:29 +0500

    5.11.2023

--- modul8_1/lib/screens/edit_product.dart
+++ modul8_1/lib/screens/edit_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modul8_1/models/product.dart';
 
 class EditProduct extends StatefulWidget {
   static const routeName = "/edit-product";
@@ -10,15 +11,91 @@ class EditProduct extends StatefulWidget {
 }
 
 class _EditProductState extends State<EditProduct> {
+  final _form = GlobalKey<FormState>();
+  final _formImg = GlobalKey<FormState>();
+
+  var _product = Product(
+    id: '',
+    title: '',
+    price: 0.0,
+    des: '',
+    imgUrl: "",
+  );
+
+  void _openInputImgeUrl(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (ctx) {
+        return AlertDialog(
+          title: Form(
+            key: _formImg,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Rasim URL'),
+              keyboardType: TextInputType.url,
+              onSaved: (newValeu) {
+                print("object");
+                _product = Product(
+                  id: '',
+                  title: _product.title,
+                  price: _product.price,
+                  des: _product.des,
+                  imgUrl: newValeu!,
+                );
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Bekor qilish"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _saveImg(context);
+              },
+              child: const Text("Saqlash"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _saveForm() {
+    print(_product.title);
+    print(_product.des);
+    print(_product.price);
+    print(_product.imgUrl);
+    _form.currentState!.save();
+  }
+
+  void _saveImg(BuildContext context) {
+    print(_product.imgUrl);
+    _formImg.currentState!.save();
+    setState(() {});
+    Navigator.of(context).pop();
+  }
+
   // final _priceFocus = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Edit Product")),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
+      appBar: AppBar(
+        title: const Text("Edit Product"),
+        actions: [
+          IconButton(
+            onPressed: _saveForm,
+            icon: const Icon(Icons.save_rounded),
+          )
+        ],
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
         child: Form(
+          key: _form,
           child: SingleChildScrollView(
+            padding: const EdgeInsets.all(15.0),
             child: Column(
               children: [
                 TextFormField(
@@ -27,30 +104,55 @@ class _EditProductState extends State<EditProduct> {
                     label: Text("Name"),
                   ),
                   textInputAction: TextInputAction.next,
+                  onSaved: (newValeu) {
+                    _product = Product(
+                      id: '',
+                      title: newValeu!,
+                      price: _product.price,
+                      des: _product.des,
+                      imgUrl: _product.imgUrl,
+                    );
+                  },
                 ),
                 const SizedBox(height: 15),
                 TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    label: Text("Price"),
-                  ),
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.next,
-                ),
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("Price"),
+                    ),
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                    onSaved: (newValeu) {
+                      _product = Product(
+                        id: '',
+                        title: _product.title,
+                        price: double.parse(newValeu!),
+                        des: _product.des,
+                        imgUrl: _product.imgUrl,
+                      );
+                    }),
                 const SizedBox(height: 15),
                 TextFormField(
-                  maxLines: 5,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    label: Text("Description"),
-                    alignLabelWithHint: true,
-                  ),
-                ),
+                    maxLines: 5,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("Description"),
+                      alignLabelWithHint: true,
+                    ),
+                    onSaved: (newValeu) {
+                      _product = Product(
+                        id: '',
+                        title: _product.title,
+                        price: _product.price,
+                        des: newValeu!,
+                        imgUrl: _product.imgUrl,
+                      );
+                    }),
                 const SizedBox(height: 15),
                 Card(
                   margin: EdgeInsets.zero,
                   child: InkWell(
-                    onTap: () {},
+                    onTap: () => _openInputImgeUrl(context),
                     borderRadius: BorderRadius.circular(5),
                     splashColor:
                         Theme.of(context).primaryColor.withOpacity(0.5),
@@ -61,9 +163,15 @@ class _EditProductState extends State<EditProduct> {
                         border: Border.all(color: Colors.grey),
                       ),
                       width: double.infinity,
-                      height: 100,
+                      height: 200,
                       alignment: Alignment.center,
-                      child: const Text("Asosy rasim URl-ni kiriting"),
+                      child: _product.imgUrl.isEmpty
+                          ? const Text("Asosy rasim URl-ni kiriting")
+                          : Image.network(
+                              _product.imgUrl,
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
                     ),
                   ),
                 ),
